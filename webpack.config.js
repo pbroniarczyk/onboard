85648a6f20c82521136e29a83860cf346f495bf5
Shenloc <pbroniarczyk@outlook.com>
2017-08-27 09:24:13 +0200

    initial commit & webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var extractTextPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },{
+                test: /\.sass$/,
+                use: extractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }, {
+                test: /\.html$/,
+                use: ['html-loader']
+            },{
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        extractTextPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
