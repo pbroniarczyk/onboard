eef578708ec18025cadcae80204d1337fd94dbf9
Shenloc <pbroniarczyk@outlook.com>
2017-08-27 11:30:13 +0200

    sass included

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ class App extends React.Component {
   render() {
     return(
       <div className="container-fluid">
-        test
+        <ul className="list">
+          <li className="list__item">item 1</li>
+          <li className="list__item">item 2</li>
+          <li className="list__item list__item--last">item 3</li>
+        </ul>
       </div>
     );
   }
