a44a5a980e3c7a5975b833785b52bac86591e1db
Shenloc <pbroniarczyk@outlook.com>
2017-08-28 21:31:08 +0200

    CoverIndex component & styling

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Components
+import CoverIndex from './cover/CoverIndex';
+
 export class Cover extends React.Component {
     render() {
         return(
             <section id="cover">
-                COVER
+                <CoverIndex />
             </section>
         );
     }
---
+++ src/components/cover/CoverIndex.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../../img/astro.png';
+
+const CoverIndex = () => {
+    return(
+        <div className="cover-wrapper">
+            <div className="cover-wrapper cover-wrapper__index">
+                <img className="cover-wrapper__astro" src="../../img/astro.png" alt="Astronaut" />
+                <h3 className="cover-wrapper__title">Witaj<br/>w Emplocity!</h3>
+                <p className="cover-wrapper__text">Aby otrzymać spersonalizowane zaproszenia od pracodawców oraz statystyki dotyczące twojej pozycji na rynku uzupełnij brakujące rubryki</p>
+            </div>
+        </div>
+    )
+}
+
+export default CoverIndex
